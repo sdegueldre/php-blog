f827253a5ac46a131695fa2d538758a406caaed5
Samuel Degueldre <samuelfdegueldre@gmail.com>
2019-02-20 09:12:50 +0100

    Change docker-compose to postgres image and get CSS up and running

--- src/routes.php
+++ src/routes.php
@@ -11,14 +11,12 @@ $twig = new \Twig\Environment($loader);
 $app->get('/[{pagename}]', function (Request $request, Response $response, array $args) {
     global $twig;
     $route = $args['pagename'];
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/$route' route");
+    $this->logger->info("Slim route: '/$route'");
 
 
     $PDO = new PDO('pgsql:host=localhost;dbname=becode', 'becode', 'becode');
-    $users = $PDO->query('SELECT user, email from users')->fetch(PDO::FETCH_ASSOC);
-    foreach ($users as $data)
-      $this->logger->info("query result: $data");
+    $users = $PDO->query('SELECT user, username, email from users')->fetchAll(PDO::FETCH_ASSOC);
+    $args['users'] = $users;
 
     // Render index view
     return $response->getBody()->write($twig->render('home.twig', $args));
