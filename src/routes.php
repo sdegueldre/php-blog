9bbcb6af3610cbf54252580ef2411d3b965038fb
Samuel Degueldre <samuelfdegueldre@gmail.com>
2019-02-26 12:17:11 +0100

    yeah no, just prefix all routes with /~{domain}, whatever

--- src/routes.php
+++ src/routes.php
@@ -4,13 +4,13 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
-$app->get('/', function (Request $request, Response $response) {
+$app->get('/~{domain}', function (Request $request, Response $response) {
     $args['pagename'] = 'Home';
     $args['users'] = $this->db->query('SELECT user, username, email FROM users')->fetchAll(PDO::FETCH_ASSOC);
     return ($this->renderer)($response, 'home.twig', $args);
 })->setName('home');
 
-$app->get('/{pagename}', function (Request $request, Response $response, array $args) {
+$app->get('/[{pagename}]', function (Request $request, Response $response, array $args) {
     $route = $args['pagename'];
     $this->logger->info("Slim: Unknown route '/$route'");
 
